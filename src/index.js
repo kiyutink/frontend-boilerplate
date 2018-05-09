bc8fbba1435c6609992664877faebc67b2084ebf
Kirill Kiyutin <kiyutin@gmail.com>
2018-05-09 18:56:13 +0300

    Add some eslint config (not finished yet)

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './app';
 
+const a = [1, 3];
+
+console.log(a);
 ReactDOM.render(<App/>, document.getElementById('root'));
